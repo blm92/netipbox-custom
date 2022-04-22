8f42be49090ffc001b2d56e94abac84803522562
Juan Pablo Garza <juanp@juanpgarza.com>
2022-04-22 17:12:02 -0300

    [13.0][ADD] nib_partner_ui

---
+++ nib_partner_ui/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'nib_partner_ui',
+    'version': '13.0.1.0.0',
+    'summary': 'Mejoras a la UI solicitadas por el cliente.',
+    'description': 'Mejoras a la UI solicitadas por el cliente.',
+    'category': 'Partner',
+    'author': 'Balmes92',
+    'company': 'Balmes92',
+    'contributors':  ['Juan Pablo Garza <jp@balmes92.com>'],
+    'website': 'https://www.balmes92.com',
+    'license': 'AGPL-3',
+    'depends': [
+            'base',
+        ],
+    'data': [
+        'views/partner_views.xml',
+        ],        
+    'installable': True,
+    'auto_install': False,
+}
