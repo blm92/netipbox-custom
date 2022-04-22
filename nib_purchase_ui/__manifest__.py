8bd4eb2b31dd68206b189b0ec9b5f60fd16de949
Juan Pablo Garza <juanp@juanpgarza.com>
2022-04-22 17:26:41 -0300

    [13.0][ADD] nib_purchase_ui

---
+++ nib_purchase_ui/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'nib_purchase_ui',
+    'version': '13.0.1.0.0',
+    'summary': 'Mejoras a la UI solicitadas por el cliente.',
+    'description': 'Mejoras a la UI solicitadas por el cliente.',
+    'category': 'Purchase',
+    'author': 'Balmes92',
+    'company': 'Balmes92',
+    'contributors':  ['Juan Pablo Garza <jp@balmes92.com>'],
+    'website': 'https://www.balmes92.com',
+    'license': 'AGPL-3',
+    'depends': [
+            'purchase',
+        ],
+    'data': [
+        'views/purchase_views.xml',
+        ],
+    'installable': True,
+    'auto_install': False,
+}
