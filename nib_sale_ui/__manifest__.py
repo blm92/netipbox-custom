e1bc0c6bcfd68b687ab4304bbbf573d01b4a8538
Juan Pablo Garza <juanp@juanpgarza.com>
2022-04-22 18:47:26 -0300

    [13.0][ADD] nib_sale_ui

---
+++ nib_sale_ui/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'nib_sale_ui',
+    'version': '13.0.1.0.0',
+    'summary': 'Mejoras a la UI solicitadas por el cliente.',
+    'description': 'Mejoras a la UI solicitadas por el cliente.',
+    'category': 'Sale',
+    'author': 'Balmes92',
+    'company': 'Balmes92',
+    'contributors':  ['Juan Pablo Garza <jp@balmes92.com>'],
+    'website': 'https://www.balmes92.com',
+    'license': 'AGPL-3',
+    'depends': [
+            'sale',
+        ],
+    'data': [
+        'views/sale_views.xml',
+        ],
+    'installable': True,
+    'auto_install': False,
+}
