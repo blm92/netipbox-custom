d3057445c13ad7d3b40e057ae39d2e9bb1ede310
Juan Pablo Garza <juanp@juanpgarza.com>
2022-07-18 19:29:30 -0300

    [15.0][ADD] nib_sale_order_quote

---
+++ nib_sale_order_quote/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 juanpgarza - Juan Pablo Garza <juanp@juanpgarza.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "nib_sale_order_quote",
+    "summary": "Mejoras al presupuesto web",
+    "version": "13.0.1.0.0",
+    "category": "Sales",
+    "website": "https://github.com/juanpgarza/sale-addons",
+    'author': 'Balmes92',
+    'company': 'Balmes92',
+    'contributors':  ['Juan Pablo Garza <jp@balmes92.com>'],    
+    "license": "AGPL-3",
+    "depends": ["sale",
+            ],
+    "data": [
+        'views/sale_portal_templates.xml',
+        ],
+    "installable": True,
+}
