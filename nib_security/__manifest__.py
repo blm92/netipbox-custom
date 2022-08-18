115136ba14c6c399a163b91ebf08cd2207b2fb88
Juan Pablo Garza <juanp@juanpgarza.com>
2022-08-18 09:55:29 -0300

    [13.0][ADD] nib_security

---
+++ nib_security/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'nib_security',
+    'version': '13.0.1.0.0',
+    'summary': 'configuraciones de seguridad.',
+    'description': 'configuraciones de seguridad.',
+    'category': 'Misc',
+    'author': 'Balmes92',
+    'company': 'Balmes92',
+    'contributors':  ['Juan Pablo Garza <jp@balmes92.com>'],
+    'website': 'https://www.balmes92.com',
+    'license': 'AGPL-3',
+    'depends': [
+            'sale',
+        ],
+    'data': [
+        'views/sale_views.xml',
+        ],
+    'installable': True,
+    'auto_install': False,
+}
